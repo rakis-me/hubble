14266aa625135255ed54a01f87818021f802da42
rakis <rakis_me@protonmail.com>
2022-02-26 23:10:29 +0000

    added user type

--- server/api.js
+++ server/api.js
@@ -8,12 +8,26 @@ const hyper = connect(Deno.env.get('HYPER'))
 
 const typeDefs = gql`
 
+type User {
+  _id: String
+  username: String
+  email: String
+  avatar: String
+  publicKey: String
+  privateKey: String
+  created: String
+  updated: String
+}
+
 type Pip {
-  _id: String,
-  name: String,
-  description: String,
-  secure: String,
+  _id: String
+  user_id: String
+  name: String
+  description: String
+  secure: String
   tags: [String]
+  created: String
+  updated: String
 }
 
 type Result {
@@ -22,10 +36,24 @@ type Result {
 
 type Query {
   findPips(criteria: String) : [Pip]
+  user(id: String) : User
+  getPublicKey(id: String) : String
+}
+
+input Nothing {
+  foo: String
+}
+
+input AvatarInput {
+  avatar: String
 }
 
 type Mutation {
   upsertPip(name: String, description: String, secure: String, tags: [String]) : Result!
+  chgUsername(username: String) : Result!
+  chgEmail(email: String) : Result!
+  chgAvatar(avatar: AvatarInput) : Result
+  generateKeys(input: Nothing) : Result
 }
 `
 
@@ -34,10 +62,28 @@ const resolvers = {
     findPips(_p, { criteria }) {
       console.log(criteria)
       return Promise.resolve([])
+    },
+    user(_p, { id }, ctx) {
+      return Promise.resolve({})
+    },
+    getPublicKey(_p, {id}) {
+      return Promise.resolve('TODO')
     }
   },
   Mutation: {
-    upsertPip(_p, { name, description, secure, tags }) {
+    upsertPip(_p, { name, description, secure, tags }, ctx) {
+      return Promise.resolve({ok: true})
+    },
+    chgUsername(_p, {username}, ctx) {
+      return Promise.resolve({ok: true})
+    },
+    chgEmail(_p, { email }, ctx) {
+      return Promise.resolve({ok: true})
+    },
+    chgAvatar(_p, { input }, ctx) {
+      return Promise.resolve({ok: true})
+    },
+    generateKeys(_p, _params, ctx) {
       return Promise.resolve({ok: true})
     }
   }
