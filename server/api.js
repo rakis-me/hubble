2e7dbbd0a7fa925cf729d881edc74afe88c40b05
rakis <rakis_me@protonmail.com>
2022-02-26 19:01:50 +0000

    update api

---
+++ server/api.js
@@ -0,0 +1,46 @@
+import { GraphQLHTTP } from "gql";
+import { makeExecutableSchema } from "graphql_tools";
+import { gql } from "graphql_tag";
+
+const typeDefs = gql`
+
+type Pip {
+  _id: String,
+  name: String,
+  description: String,
+  secure: String,
+  tags: [String]
+}
+
+type Result {
+  ok: Boolean
+}
+
+type Query {
+  findPips(criteria: String) : [Pip]
+}
+
+type Mutation {
+  upsertPip(name: String, description: String, secure: String, tags: [String]) : Result!
+}
+`
+
+const resolvers = {
+  Query: {
+    findPips(_p, { criteria }) {
+      console.log(criteria)
+      return Promise.resolve([])
+    }
+  },
+  Mutation: {
+    upsertPip(_p, { name, description, secure, tags }) {
+      return Promise.resolve({ok: true})
+    }
+  }
+}
+
+export const graphql = (req) =>
+  GraphQLHTTP({
+    schema: makeExecutableSchema({ resolvers, typeDefs }),
+    graphiql: true,
+  })(req);
--- server/mod.js
+++ server/mod.js
@@ -1,1 +1,11 @@
 import { serve } from "std/http/server.ts"
+import { graphql } from './api.js'
+
+const GQL = new URLPattern({pathname: '/graphql'})
+
+serve((req) => {
+  if (GQL.exec(req.url)) {
+    return graphql(req)
+  }
+  return new Response('Not Found', { status: 404})
+})
